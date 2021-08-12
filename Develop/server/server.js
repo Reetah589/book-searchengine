a2cd1af45a8fad63a502ffe6385adfe4b362713a
reetah589 <reetah589@gmail.com>
2021-08-12 13:19:11 -0500

    testing gql api code

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -13,6 +13,8 @@ const server = new ApolloServer({
   resolvers
 });
 
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -24,7 +26,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
 
 // Tried NPM RUN WATCH in terminal
